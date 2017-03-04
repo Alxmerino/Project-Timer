a8c2cb21104ba59dfcdde8764ea1bce1ca2ca854
Rene Merino <rmerino@amayamedia.com>
2017-03-03 22:35:58 -0500

    Added gulpfile and DotFiles

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+'use strict';
+
+let configFile  = __dirname + '/configs/client_config.json';
+let gulp        = require('gulp');
+let gulpTasks   = require('gulp-generic-build')(gulp, configFile);
+let paths       = require(configFile);
+let sourcemaps  = require("gulp-sourcemaps");
+
+
+// New ES6 project with Babel, Browserify & Gulp
+// https://gist.github.com/danharper/3ca2273125f500429945
+let source = require('vinyl-source-stream');
+let buffer = require('vinyl-buffer');
+let browserify = require('browserify');
+let uglify = require('gulp-uglify');
+let watchify = require('watchify');
+let babel = require('babelify');
+
+function compile(watch) {
+    let bundler = watchify(browserify(paths.source.js + '/app.js', { debug: true }).transform(babel));
+
+    function rebundle() {
+        bundler.bundle()
+        .on('error', function(err) { console.error(err); this.emit('end'); })
+        .pipe(source('app.min.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(paths.dest.js));
+    }
+
+    if (watch) {
+        bundler.on('update', function() {
+            console.log('-> bundling...');
+            rebundle();
+        });
+    }
+
+    rebundle();
+}
+
+function watch() {
+    return compile(true);
+};
+
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
+
+gulp.task('default', ['build'], function() {
+    // @TODO: Look up why build process is not exiting
+    process.exit(0);
+});
