3470dc3f3e7e18335bf987c3b16b2c993f3571ca
Rene Merino <rmerino@amayamedia.com>
2017-03-04 02:47:46 -0500

    Update timer row from props

--- assets/js/components/TimerRow.js
+++ assets/js/components/TimerRow.js
@@ -2,13 +2,17 @@ const React     = require('react');
 
 class TimerRow extends React.Component {
     render() {
+
+        let {timer} = this.props;
+        let timerStatus = (timer.isPaused) ? 'pause' : 'play';
+
         return (
             <li className="list-group-item">
                 <a href="#" className="timer-close"><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></a>
-                <div className="timer-title">Dapibus ac facilisis in</div>
+                <div className="timer-title">{timer.title}</div>
                 <div className="timer-stats">
                     <span className="timer-current">01:23:34</span> / <span className="timer-planned">2:00:00</span>
-                    <button type="button" className="timer-play btn btn-info btn-xs"><span className="glyphicon glyphicon-play" aria-hidden="true"></span></button>
+                    <button type="button" className={`timer-${timerStatus} btn btn-info btn-xs`}><span className={`glyphicon glyphicon-${timerStatus}`} aria-hidden="true"></span></button>
                 </div>
             </li>
         );
