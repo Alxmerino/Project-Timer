a5e925abac9364acf3d3953eeda8153257019806
Rene Merino <rmerino@amayamedia.com>
2017-03-05 13:12:29 -0500

    Build timer on component mount and add base timer toggle

--- assets/js/components/TimerRow.js
+++ assets/js/components/TimerRow.js
@@ -1,38 +1,85 @@
-const React     = require('react');
-const $         = require('jquery');
+const React                 = require('react');
+const $                     = require('jquery');
+const moment                = require('moment');
+const momentDurationFormat  = require('moment-duration-format');
+const Logger                = require('../components/Logger');
+const Timer                 = require('../components/Timer');
 
 class TimerRow extends React.Component {
-    render() {
 
-        let {timer} = this.props;
-        let timerStatus = (timer.isPaused) ? 'pause' : 'play';
+    constructor() {
+        super();
+
+        // Debugger
+        this.debug = new Logger('TimerRow');
+
+        // Bind methods
+        this.handleCloseClick = this.handleCloseClick.bind(this);
+        this.toggleTimer = this.toggleTimer.bind(this);
+    }
+
+    componentDidMount() {
+        // Build timer whent he component has mounted
+        this.debug.log('Build timer');
+
+        let {rowTimer, buildTimer} = this.props;
+
+        buildTimer(rowTimer);
+    }
+
+    render() {
+        let {rowTimer} = this.props;
+        let timerStatus = (rowTimer.isPaused) ? 'pause' : 'play';
 
         return (
             <li className="list-group-item">
                 <a
                     href="#"
                     className="timer-close"
-                    data-id={timer.id}
-                    onClick={this.handleClick.bind(this)}
+                    data-id={rowTimer.id}
+                    onClick={this.handleCloseClick}
                 >
                     <span className="glyphicon glyphicon-remove" aria-hidden="true"></span>
                 </a>
-                <div className="timer-title">{timer.title}</div>
+                <div className="timer-title">{rowTimer.title}</div>
                 <div className="timer-stats">
-                    <span className="timer-current">0:00:00</span> / <span className="timer-planned">{timer.planned}</span>
-                    <button type="button" className={`timer-${timerStatus} btn btn-info btn-xs`}><span className={`glyphicon glyphicon-${timerStatus}`} aria-hidden="true"></span></button>
+                    <span className="timer-current" ref={(el) => this.timerText = el}>0:00</span> / <span className="timer-planned">{this.formatTime(rowTimer.planned)}</span>
+                    <button
+                        type="button"
+                        className={`timer-${timerStatus} btn btn-info btn-xs`}
+                        onClick={this.toggleTimer}
+                    >
+                        <span className={`glyphicon glyphicon-${timerStatus}`} aria-hidden="true"></span>
+                    </button>
                 </div>
             </li>
         );
     }
 
-    handleClick(e) {
+    handleCloseClick(e) {
         e.preventDefault();
 
         let id = $(e.currentTarget).data('id');
 
         // Remove timer
         this.props.removeTimer(id);
+    }
+
+    toggleTimer() {
+        let {timer} = this.props;
+
+        var thisTimer = BuildTimer(null, timer.planned)
+        console.log('THIS', thisTimer)
+
+        setTimeout(function() {
+            console.log('STOP');
+            thisTimer.stop();
+        }, 5000);
+        // this.props.toggleTimer(id);
+    }
+
+    formatTime(time) {
+        return moment.duration(time, 'minutes').format('h:mm', { trim: false });
     }
 }
 
