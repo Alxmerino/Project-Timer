70576ffe8a6bff394375f2916b1af6c536b66c9c
Rene Merino <rmerino@amayamedia.com>
2017-03-05 13:10:47 -0500

    Add base moment timer

---
+++ assets/js/components/Timer.js
@@ -0,0 +1,28 @@
+const moment    = require('moment');
+const timer    = require('moment-timer');
+
+// var intervalContainer = document.getElementById("interval");
+// var startTick = new Date().getTime();
+// var interval = moment.duration(1, "seconds").timer({
+//     loop: true,
+//     wait: 1000,
+//     executeAfterWait: true
+// },
+// function() {
+//     console.log('Runnine', new Date().getTime())
+//     // intervalContainer.innerHTML += "Callback fired "
+//     // + (new Date().getTime() - startTick)
+//     // + "ms after script was started.<br>";
+// });
+
+function BuildTimer(callback) {
+    // Convert minutes to seconds
+    // duration = duration * 60;
+
+    return moment.duration(1, "seconds").timer({
+        loop: true,
+        start: false
+    }, callback);
+}
+
+module.exports = BuildTimer;
