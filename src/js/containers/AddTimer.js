1810e528cb230c74e707300e797ff38a5b587f13
Rene Merino <rmerino@amayamedia.com>
2017-03-08 21:08:40 -0500

    Started adding back form methods

--- src/js/containers/AddTimer.js
+++ src/js/containers/AddTimer.js
@@ -1,16 +1,49 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import $ from 'jquery'
 import {addTimer} from '../actions'
 
 let AddTimer = ({ dispatch }) => {
+    let formInputs = {};
+
+    let handleSubmit = (e) => {
+        e.preventDefault();
+
+        // if (validTimerForm())
+        console.log('VALID', validTimerForm())
+        dispatch(addTimer({timer: 'foo'}));
+    }
+
+    /**
+     *
+     * @desc Determines if we can add a timer or not, returns valid
+     *       if at least the title is not empty, time can be left
+     *       empty in which case a counter should be started
+     *       instead of a timer.
+     *
+     * @return {Boolean}
+     *
+     */
+    let validTimerForm = () => {
+        if (formInputs.title.value !== "") {
+            $(formInputs.title).parent().removeClass('has-error');
+
+            return true;
+        } else {
+            $(formInputs.title).parent().addClass('has-error');
+
+            return false;
+        }
+    }
 
     return (
-        <form className="row" onSubmit="">
+        <form className="row" onSubmit={handleSubmit}>
             <div className="form-group col-xs-7">
                 <input
                     type="text"
                     placeholder="What are you working on today?"
                     className="form-control"
+                    ref={(input) => formInputs.title = input}
                 />
             </div>
             <div className="form-group col-xs-5 col-sm-3">
@@ -18,6 +51,7 @@ let AddTimer = ({ dispatch }) => {
                     type="text"
                     placeholder="How long?"
                     className="form-control"
+                    ref={(input) => formInputs.time = input}
                 />
             </div>
             <div className="form-group col-xs-12 col-sm-2">
