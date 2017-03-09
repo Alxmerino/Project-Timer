631f95bcda2f04a4b4ffbfc253e52df38b7e4e47
Rene Merino <rmerino@amayamedia.com>
2017-03-08 23:19:26 -0500

    Destroy timer item from state

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -5,6 +5,15 @@ export default function reducer(state={}, action) {
             let newTimer = action.payload.timer;
             return Object.assign({}, state, {[newTimer.id]: newTimer});
         }
+
+        case "TIMER_DESTROY": {
+            let id = action.payload
+            let newState = Object.assign({}, state);
+
+            delete newState[id];
+
+            return newState;
+        }
     }
 
     return state;
