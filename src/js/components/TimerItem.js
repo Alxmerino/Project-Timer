bac0d5e4fb5b5f5a003ff2229041cf2ffb349bc0
Rene Merino <rmerino@amayamedia.com>
2017-03-08 23:20:02 -0500

    Handle onClose, started and title prop

--- src/js/components/TimerItem.js
+++ src/js/components/TimerItem.js
@@ -1,13 +1,19 @@
 import React, { PropTypes } from 'react'
 
-const TimerItem = ({onClose, onToggle, status, name}) => {
-    let active = (status) ? 'active' : 'inactive';
-    let timerStatus = (status) ? 'pause' : 'play';
+const TimerItem = ({onClose, onToggle, started, title}) => {
+    let active = (started) ? 'active' : 'inactive';
+    let timerStatus = (started) ? 'pause' : 'play';
 
     return (
         <li className={`list-group-item ${active}`}>
-            <a href="#" className="timer-close"><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></a>
-            <div className="timer-title">{name}</div>
+            <a
+                href="#"
+                className="timer-close"
+                onClick={onClose}
+            >
+                <span className="glyphicon glyphicon-remove" aria-hidden="true"></span>
+            </a>
+            <div className="timer-title">{title}</div>
             <div className="timer-stats">
                 <span className="timer-current">01:23:34</span> / <span className="timer-planned">2:00:00</span>
                 <button type="button" className={`timer-${timerStatus} btn btn-info btn-xs`}><span className={`glyphicon glyphicon-${timerStatus}`} aria-hidden="true"></span></button>
@@ -19,8 +25,8 @@ const TimerItem = ({onClose, onToggle, status, name}) => {
 TimerItem.propTypes = {
     // onClose: PropTypes.func.isRequired,
     // onToggle: PropTypes.func.isRequired,
-    status: PropTypes.bool.isRequired,
-    name: PropTypes.string.isRequired
+    // status: PropTypes.bool.isRequired,
+    // title: PropTypes.string.isRequired
 }
 
 export default TimerItem;
