d1e0a7d48772a6ef6d0172ae3510435a89698984
Rene Merino <rmerino@amayamedia.com>
2017-03-08 20:38:32 -0500

    Turn main app into a presentational component

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,67 +1,38 @@
 import React, { Component} from 'react'
-import $ from 'jquery'
-import moment from 'moment'
-import _ from 'underscore'
+import AddTimer from '../containers/AddTimer'
 
-// React Components
-// let TimerRow    = require('../components/TimerRow');
+let App = () => {
+    return (
+        <div className="main-wrapper">
 
-class App extends Component {
-
-    render() {
-        return (
-            <div className="main-wrapper">
-
-                <div className="container">
-                    <div className="header clearfix">
-                        <div className="row">
-                            <div className="col-xs-12">
-                                <h1>Projects Timer</h1>
-                            </div>
-                        </div>
-                        <form className="row" onSubmit="">
-                            <div className="form-group col-xs-7">
-                                <input
-                                    type="text"
-                                    placeholder="What are you working on today?"
-                                    className="form-control"
-                                    ref=""
-                                />
-                            </div>
-                            <div className="form-group col-xs-5 col-sm-3">
-                                <input
-                                    type="text"
-                                    placeholder="How long?"
-                                    className="form-control"
-                                    ref=""
-                                />
-                            </div>
-                            <div className="form-group col-xs-12 col-sm-2">
-                                <button type="submit" className="btn btn-success">Add Timer</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-
-                <div className="container">
+            <div className="container">
+                <div className="header clearfix">
                     <div className="row">
                         <div className="col-xs-12">
-                            <ul className="list-group">
-                            </ul>
+                            <h1>Projects Timer</h1>
                         </div>
                     </div>
+                    <AddTimer />
                 </div>
-
-                <footer className="container footer">
-                    <div className="row">
-                        <p className="col-xs-12">&copy; 2016 Amaya Media</p>
-                    </div>
-                </footer>
-
             </div>
-        )
-    }
 
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-12">
+                        <ul className="list-group">
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <footer className="container footer">
+                <div className="row">
+                    <p className="col-xs-12">&copy; 2016 Amaya Media</p>
+                </div>
+            </footer>
+
+        </div>
+    )
 }
 
 export default App
