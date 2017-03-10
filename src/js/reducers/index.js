63c6816fb31806530b6a0e120833fae0b35a7673
Rene Merino <rmerino@amayamedia.com>
2017-03-09 19:32:08 -0500

    Add TIMER_TOGGLE reducer

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,18 +1,26 @@
 export default function reducer(state={}, action) {
 
     switch(action.type) {
-        case "TIMER_ADD": {
+        case 'TIMER_ADD': {
             let newTimer = action.payload.timer;
             return Object.assign({}, state, {[newTimer.id]: newTimer});
         }
 
-        case "TIMER_DESTROY": {
+        case 'TIMER_DESTROY': {
             let id = action.payload
             let newState = Object.assign({}, state);
 
             delete newState[id];
 
             return newState;
+        }
+
+        case 'TIMER_TOGGLE': {
+            let id = action.payload;
+            let timer = state[id];
+                timer.started = !timer.started;
+
+            return Object.assign({}, state, {id: timer});
         }
     }
 
