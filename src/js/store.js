140e266f74e6d4ddad81394e1b935f0fa8e38304
Rene Merino <rmerino@amayamedia.com>
2017-03-12 18:15:35 -0400

    Not using this middleware for now

--- src/js/store.js
+++ src/js/store.js
@@ -1,11 +1,9 @@
 import { applyMiddleware, createStore } from 'redux'
 import logger from 'redux-logger'
-import thunk from 'redux-thunk'
-import promise from 'redux-promise-middleware'
 
 import reducer from './reducers'
 
 // Apply middleware
-const middleware = applyMiddleware(promise(), thunk, logger());
+const middleware = applyMiddleware(logger());
 
 export default createStore(reducer, middleware);
