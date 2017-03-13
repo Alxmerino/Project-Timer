fd15f64e9998a3fb2f676b9b2e7594532f201a9a
Rene Merino <rmerino@amayamedia.com>
2017-03-12 20:09:19 -0400

    Dispart inital APP_LOADED event

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+
 import store from './store'
 import App from './components/App'
+import Logger from './components/Logger'
+
 const app = document.getElementById('app')
+const Debug = new Logger('App');
+
+// Distpatch initial app load
+store.dispatch({type: 'APP_LOADED'});
 
 render(<Provider store={store}>
     <App/>
