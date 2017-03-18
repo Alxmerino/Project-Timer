be2924eba76be85a8b974041835ad143bb702f3b
Rene Merino <rmerino@amayamedia.com>
2017-03-18 12:55:48 -0400

    Spaces

--- tests/actions.test.js
+++ tests/actions.test.js
@@ -2,24 +2,26 @@ import moment from 'moment';
 
 import * as actions from '../src/js/actions'
 
-/** Test TIMER_ADD action */
+/** Test Actions */
 describe('actions', () => {
-  it('should create an action to add a timer', () => {
-    let id = moment.now();
-    const timer = {
-        id,
-        title: 'New title',
-        createdTime: id,
-        started: false,
-        duration: 0,
-        plannedTime: '00:00:00',
-    }
 
-    const expectedAction = {
-      type: 'TIMER_ADD',
-      payload: timer
-    }
+    it('should create an action to add a timer', () => {
+        let id = moment.now();
+        const timer = {
+            id,
+            title: 'New title',
+            createdTime: id,
+            started: false,
+            duration: 0,
+            plannedTime: '00:00:00',
+        };
 
-    expect(actions.addTimer(timer)).toEqual(expectedAction);
-  })
-})
+        const expectedAction = {
+            type: 'TIMER_ADD',
+            payload: timer
+        };
+
+        expect(actions.addTimer(timer)).toEqual(expectedAction);
+    });
+
+});
