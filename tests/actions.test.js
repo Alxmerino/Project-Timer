e9a1b5fc38e25ca826ce9471d45f9a42f9f67b8c
Rene Merino <rmerino@amayamedia.com>
2017-03-18 12:54:28 -0400

    Add TIMER_ADD test

---
+++ tests/actions.test.js
@@ -0,0 +1,25 @@
+import moment from 'moment';
+
+import * as actions from '../src/js/actions'
+
+/** Test TIMER_ADD action */
+describe('actions', () => {
+  it('should create an action to add a timer', () => {
+    let id = moment.now();
+    const timer = {
+        id,
+        title: 'New title',
+        createdTime: id,
+        started: false,
+        duration: 0,
+        plannedTime: '00:00:00',
+    }
+
+    const expectedAction = {
+      type: 'TIMER_ADD',
+      payload: timer
+    }
+
+    expect(actions.addTimer(timer)).toEqual(expectedAction);
+  })
+})
