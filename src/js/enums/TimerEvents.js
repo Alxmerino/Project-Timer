428f3e7bd02fa5bdf1b7fe47fb3ff60a6b30d777
Rene Merino <rmerino@amayamedia.com>
2017-05-07 18:18:38 -0400

    Renamed events file name

---
+++ src/js/enums/TimerEvents.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * @desc List of enums used throughout the app. It's in CommonJS-like
+ * format as some of these are used in the electron side of the app
+ * @type {Object}
+ *
+ */
+/* eslint-disable no-undef */
+module.exports = {
+    QUIT:                   '__app_quit',
+
+    /** Timer Events */
+    TIMER_ADD:                  '__timer_add',
+    TIMER_DESTROY:              '__timer_destro',
+    TIMER_START:                '__timer_start',
+    TIMER_STOP:                 '__timer_stop',
+    TIMER_TOGGLE:               '__timer_toggle',
+    TIMER_UPDATE:               '__timer_update',
+    TIMER_TITLE_CHANGE_ON:      '__timer_title_change_on',
+    TIMER_TITLE_CHANGE_OFF:     '__timer_title_change_off',
+    TIMER_TITLE_UPDATE:         '__timer_title_update',
+    TIMER_DURATION_ON:          '__timer_duration_on',
+    TIMER_DURATION_OFF:         '__timer_duration_off',
+    TIMER_DURATION_UPDATE:      '__timer_duration_update',
+    TIMER_PLANNED_ON:           '__timer_planned_on',
+    TIMER_PLANNED_OFF:          '__timer_planned_off',
+    TIMER_PLANNED_UPDATE:       '__timer_planned_update',
+    TIMER_DESCRIPTION_ON:       '__timer_description_on',
+    TIMER_DESCRIPTION_OFF:      '__timer_description_off',
+    TIMER_DESCRIPTION_UPDATE:   '__timer_description_update',
+    TIMER_RESET:                '__timer_reset'
+};
