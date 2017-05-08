b2278e54d7671923c3d70ef00725c6967067e564
Rene Merino <rmerino@amayamedia.com>
2017-05-07 20:08:37 -0400

    Remove quit logic from main App component

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -5,33 +5,8 @@ import Summary              from '../containers/Summary';
 import AppEvents            from '../enums/AppEvents';
 import AppIcon              from '../components/AppIcon';
 import Options              from '../components/Options';
-import { isElectronApp }    from '../utils/utils';
-
-// Require ipcRenderer only in electron app
-const { ipcRenderer }   = (isElectronApp()) ? window.require('electron') : {};
 
 let App = () => {
-
-    /**
-     *
-     * @desc Handle electron quit event for electron App
-     *
-     */
-    let onQuit = () => {
-        ipcRenderer.send('async-message', AppEvents.QUIT);
-    };
-
-    let renderQuitButton = () => {
-        if (isElectronApp()) {
-            return (
-                <p className="col-xs-12">
-                    <button onClick={onQuit} className="btn btn-success">Quit</button>
-                </p>
-            );
-        } else {
-            return null;
-        }
-    };
 
     return (
         <div className="main-wrapper">
@@ -60,7 +35,6 @@ let App = () => {
 
             <footer className="container footer">
                 <div className="row">
-                    {renderQuitButton()}
                     <p className="col-xs-12">&copy; 2017 <a href="https://www.amayamedia.com" target="_blank">Amaya Media</a></p>
                 </div>
             </footer>
