0a50101ab6e38928f374b323a998a3b96578adf5
Rene Merino <rmerino@amayamedia.com>
2017-05-22 16:49:46 -0400

    Added new helper to get ping interval

--- src/js/helpers/index.js
+++ src/js/helpers/index.js
@@ -1,11 +1,14 @@
+const _      = require('underscore');
 const moment = require('moment');
 require('moment-duration-format');
 
 /**
+ *
  * @desc Format duration time string
  * @param  {Number} time
  * @param  {string} format
  * @return {String}
+ *
  */
 const formatTime = (time, format) => {
     format = (typeof(format) !== 'undefined') ? format : 'minutes';
@@ -14,10 +17,12 @@ const formatTime = (time, format) => {
 };
 
 /**
+ *
  * @desc Get total timer duration in specified format
  * @param {Number} duration
  * @param  {String} format
  * @return {Number}
+ *
  */
 const getTimeIn = (duration, format) => {
     if (!duration) {
@@ -29,6 +34,10 @@ const getTimeIn = (duration, format) => {
             return ((duration/1000) / 60);
         }
 
+        case 'milliseconds': {
+            return (duration*1000);
+        }
+
         case 'seconds':
         default: {
             return (duration/1000);
@@ -36,7 +45,36 @@ const getTimeIn = (duration, format) => {
     }
 };
 
+/**
+ *
+ * @desc Get minute interval to ping electron app when app
+ *       is in overtime
+ * @param  {Number} pingArray
+ * @return {Number}
+ *
+ */
+const getIpcPingInterval = (ping) => {
+    let pingInterval;
+    // @TODO Hacky way so it keeps returning last interval
+    let lastPingInterval = 35;
+    let pingIntervalArray = [5, 15, 30, 45, 60];
+    let pingIndex = _.indexOf(pingIntervalArray, ping);
+
+    // We just started pinging
+    if (typeof(ping) === 'undefined' || ping === null) {
+        return pingIntervalArray[0];
+    }
+
+    // We maxed out the ping array
+    if (ping === lastPingInterval) {
+        return lastPingInterval;
+    }
+
+    return pingIntervalArray[pingIndex+1] || lastPingInterval;
+};
+
 module.exports = {
     getTimeIn,
-    formatTime
+    formatTime,
+    getIpcPingInterval
 };
