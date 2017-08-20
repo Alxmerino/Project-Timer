183371110d3dab627cf18085648c820f2353b42b
Rene Merino <rmerino@amayamedia.com>
2017-08-20 19:13:31 -0400

    Use timer index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+const React            = require('react');
+const { render }       = require('react-dom');
+const { Provider }     = require('react-redux');
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App              = require('./js/components/App');
+const Logger           = require('./js/components/Logger');
+const PersistStore     = require('./js/helpers/PersistStore');
+const store            = require('./js/store');
+
+/* eslint-disable no-unused-vars */
+const Debug = new Logger('App');
+/* eslint-enable no-unused-vars */
+new PersistStore();
+
+// Distpatch initial app load
+store.dispatch({type: 'APP_LOADED'});
+
+render(<Provider store={store}>
+    <App/>
+</Provider>, document.getElementById('app'));
