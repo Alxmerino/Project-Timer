7f3a5bda3c03c3713776d89718690df248be1fca
Rene Merino <rmerino@amayamedia.com>
2017-08-21 00:26:11 -0400

    Decouple localStorage

--- src/js/helpers/Storage.js
+++ src/js/helpers/Storage.js
@@ -1,7 +1,27 @@
-const { xStore } = require('xStore');
+const xStore = require('xStore');
 
 let Storage = (function() {
-    let store = new xStore('projectTimer:', window.localStorage);
+    let store = {};
+    let _store;
+    if (typeof xStore === 'object') {
+        _store = new xStore.xStore('projectTimer:', window.localStorage);
+    }
+
+    if (typeof xStore === 'function') {
+        _store = new xStore('projectTimer:', window.localStorage);
+    }
+
+    store.set = function(key, value) {
+        return _store.set(key, value);
+    }
+
+    store.remove = function(keys) {
+        return _store.remove(keys);
+    }
+
+    store.all = function() {
+        return _store.all();
+    }
 
     return store;
 })();
