551b693f8ae587f715bf6db346748b6df7ffbfad
Rene Merino <rmerino@amayamedia.com>
2017-10-01 13:11:31 -0400

    Add app event enums

--- src/js/enums/AppEvents.js
+++ src/js/enums/AppEvents.js
@@ -7,7 +7,17 @@
  */
 /* eslint-disable no-undef */
 module.exports = {
+    READY:          'ready',
+    ACTIVATE:       'activate',
+    SHOW:           'show',
+    HIDE:           'hide',
+
+    TRAY_CLICKED:   '__app_tray_clicked',
+
     QUIT:           '__app_quit',
     FOCUSED:        '__app_focused',
     MENU_TOGGLE:    '__app_menu_open',
+
+    CLOSE:          '__app_close',
+    MINIMIZE:       '__app_minimize',
 };
