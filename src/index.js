b1352bcae4dfca03291602266e2120b4f911a8e1
Rene Merino <rmerino@amayamedia.com>
2018-01-30 11:46:30 -0500

    Connect app to router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-const React            = require('react');
-const { render }       = require('react-dom');
-const { Provider }     = require('react-redux');
+const React               = require('react');
+const { Route }           = require('react-router');
+const { render }          = require('react-dom');
+const { Provider }        = require('react-redux');
+const { ConnectedRouter } = require('react-router-redux');
 
-const App              = require('./js/components/App');
-const Logger           = require('./js/utils/Logger');
-const PersistStore     = require('./js/helpers/PersistStore');
-const store            = require('./js/store');
+const App                 = require('./js/components/App');
+const Landing             = require('./js/components/Landing');
+const LoginJira           = require('./js/components/login/Jira');
+const Logger              = require('./js/utils/Logger');
+const PersistStore        = require('./js/helpers/PersistStore');
+const { history, store }  = require('./js/store');
 
 /* eslint-disable no-unused-vars */
 const Debug = new Logger('App');
@@ -13,5 +17,15 @@ const Debug = new Logger('App');
 new PersistStore();
 
 render(<Provider store={store}>
-    <App/>
+    <ConnectedRouter history={history}>
+        <div>
+            <Route exact path="/" component={Landing}/>
+            <Route path="/login/jira" component={LoginJira} />
+            <Route path="/app" component={App}/>
+        </div>
+    </ConnectedRouter>
+    {/* <App/> */}
 </Provider>, document.getElementById('app'));
+
+/** Distpatch initial app load */
+store.dispatch({type: 'APP_LOADED'});
